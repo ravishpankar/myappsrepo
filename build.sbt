a54bb77aa8a989186cc08e719b68f26be84f0d18
ravishpankar <rapvishankar@yahoo.in>
2016-05-22 11:26:36 +0530

    Postgre sql driver added.

--- build.sbt
+++ build.sbt
@@ -6,11 +6,14 @@ lazy val root = (project in file(".")).enablePlugins(PlayJava, PlayEbean)
 
 scalaVersion := "2.11.7"
 
+resolvers +=
+  "mvnrepo" at "http://mvnrepository.com/artifact/"
+
 libraryDependencies ++= Seq(
   javaJdbc,
   cache,
   javaWs,
-  "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
+  "org.postgresql" % "postgresql" % "9.4.1208.jre7"
 )
 
 
--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,22 +3,21 @@
 
 # --- !Ups
 
-create table post (
-  id                            bigint auto_increment not null,
-  post                          varchar(500),
-  posted_on                     datetime(6),
-  user_id                       varchar(16),
-  constraint pk_post primary key (id)
-);
-
-
 create table comment (
-  id                            bigint auto_increment not null,
+  id                            bigserial not null,
   post_id                       varchar(255),
   comment                       varchar(500),
-  commented_on                  datetime(6),
+  commented_on                  timestamp,
   user_id                       varchar(16),
   constraint pk_comment primary key (id)
+);
+
+create table post (
+  id                            bigserial not null,
+  post                          varchar(500),
+  posted_on                     timestamp,
+  user_id                       varchar(16),
+  constraint pk_post primary key (id)
 );
 
 create table user (
@@ -29,8 +28,9 @@ create table user (
 
 # --- !Downs
 
-drop table if exists comment;
+drop table if exists comment cascade;
 
-drop table if exists post;
+drop table if exists post cascade;
 
-drop table if exists user;
+drop table if exists user cascade;
+
