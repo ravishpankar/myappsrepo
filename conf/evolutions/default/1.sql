168d19540cf362cc4b72797e70eb084980f49def
ravishpankar <rapvishankar@yahoo.in>
2016-06-15 09:55:11 +0530

    Postgre sql generation.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,4 +1,5 @@
-
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
 
 # --- !Ups
 
@@ -13,22 +14,20 @@ create table comment (
 create sequence comment_seq;
 
 create table grp (
-  id                            integer not null,
+  id                            serial not null,
   group_id                      varchar(255),
   owner_id                      varchar(255),
   last_updated                  timestamp not null,
   constraint pk_grp primary key (id)
 );
 insert into grp values (0, 'none', 'any', now());
-create sequence grp_seq;
 
 create table grp_usr (
-  id                            integer not null,
+  id                            serial not null,
   user_id                       varchar(255),
   group_id                      integer,
   constraint pk_grp_usr primary key (id)
 );
-create sequence grp_usr_seq;
 
 create table post (
   id                            bigint not null,
@@ -43,13 +42,12 @@ create table post (
 create sequence post_seq;
 
 create table usr (
-  id                            integer not null,
+  id                            serial not null,
   user_id                       varchar(255),
   pwd                           varchar(255),
   is_admin                      boolean,
   constraint pk_usr primary key (id)
 );
-create sequence usr_seq;
 
 alter table comment add constraint fk_comment_post_id foreign key (post_id) references post (id) on delete restrict on update restrict;
 create index ix_comment_post_id on comment (post_id);
@@ -60,24 +58,21 @@ create index ix_grp_usr_group_id on grp_usr (group_id);
 
 # --- !Downs
 
-alter table comment drop constraint if exists fk_comment_post_id;
+alter table if exists comment drop constraint if exists fk_comment_post_id;
 drop index if exists ix_comment_post_id;
 
-alter table grp_usr drop constraint if exists fk_grp_usr_group_id;
+alter table if exists grp_usr drop constraint if exists fk_grp_usr_group_id;
 drop index if exists ix_grp_usr_group_id;
 
-drop table if exists comment;
+drop table if exists comment cascade;
 drop sequence if exists comment_seq;
 
-drop table if exists grp;
-drop sequence if exists grp_seq;
+drop table if exists grp cascade;
 
-drop table if exists grp_usr;
-drop sequence if exists grp_usr_seq;
+drop table if exists grp_usr cascade;
 
-drop table if exists post;
+drop table if exists post cascade;
 drop sequence if exists post_seq;
 
-drop table if exists usr;
-drop sequence if exists usr_seq;
+drop table if exists usr cascade;
 
